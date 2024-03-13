602af3568fa13268e4360a6971494f0effb97f96
Asim Shah <asimshah2111@gmail.com>
2024-03-14 00:47:37 +0530

    code formatting dome

--- server/server.js
+++ server/server.js
@@ -1,31 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import connectToDatabase from './services/mongooseService.js';
-import RegisterRouter from './routes/registerRoute.js';
-import loginRouter from './routes/loginRoute.js';
-import uploadRouter from './routes/uploadRoute.js';
-import corsOptions from './config/corsOptions.js';
-import generelRouter from './routes/generelRoute.js';
-import ttsRouter from './routes/ttsRoute.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import connectToDatabase from './services/mongooseService.js'
+import RegisterRouter from './routes/registerRoute.js'
+import loginRouter from './routes/loginRoute.js'
+import uploadRouter from './routes/uploadRoute.js'
+import corsOptions from './config/corsOptions.js'
+import generelRouter from './routes/generelRoute.js'
+import ttsRouter from './routes/ttsRoute.js'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-connectToDatabase();
+connectToDatabase()
 
-app.use(express.json());
-app.use(cors(corsOptions));
+app.use(express.json())
+app.use(cors(corsOptions))
 
-app.use('/register', RegisterRouter);
-app.use('/login', loginRouter);
-app.use('/upload', uploadRouter);
-app.use('/general', generelRouter);
-app.use('/textToSpeech', ttsRouter);
+app.use('/register', RegisterRouter)
+app.use('/login', loginRouter)
+app.use('/upload', uploadRouter)
+app.use('/general', generelRouter)
+app.use('/textToSpeech', ttsRouter)
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
