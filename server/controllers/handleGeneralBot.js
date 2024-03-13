2665720dda6f8a36e29247270777035e6f0c4925
Asim Shah <asimshah2111@gmail.com>
2024-03-14 04:55:17 +0530

    made normal gpt call in generel bot temporarly

--- server/controllers/handleGeneralBot.js
+++ server/controllers/handleGeneralBot.js
@@ -3,19 +3,25 @@ import openAiBot from '../services/openAiService.js'
 const handleGeneralBot = async (req, res) => {
     const { prompt } = req.body
     try {
-        const result = await openAiBot(
-            ` is the question releted to travelling if releted the write only yes
-            dont write any other word otherwise answer with only no dont write any other word
-            the question is: ${prompt}
-            `
-        )
+        // const result = await openAiBot(
+        //     ` "Is the question related to travel? 
+        //      ${prompt}" 
+        //     Respond with 'yes' if the question is related to travel; 
+        //     otherwise, respond with 'no' (omit any other words).
+        //     `
+        // )
+        // console.log(result)
+        // if (result.toLocaleLowerCase() === 'yes') {
+        //     console.log('awaiting openai');
+        //     const response = await openAiBot(`
+        //     ${prompt}`)
+        //     res.send(response)
+        // } else {
+        //     res.send(`cannot answer the following question`)
+        // }
+        const result = await openAiBot(`please answer this question :${prompt}`)
         console.log(result)
-        if (result === 'Yes') {
-            const response = await openAiBot(`${prompt}`)
-            res.send(response)
-        } else {
-            res.send(`cannot answer the following question`)
-        }
+        res.send(result)
     } catch (error) {
         console.log(error)
         res.send(`error while generating prompt`)
