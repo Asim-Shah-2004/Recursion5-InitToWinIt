6dfc9f38c7da44f5a7cd949c7952a7078a9a1181
SoulDev07 <mayankravariya@gmail.com>
2024-03-14 03:56:08 +0530

    update dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,12 +9,27 @@ import './Dashboard.css'
 
 const SERVERURL = import.meta.env.VITE_SERVERURL;
 
-const handleOnGenerateClick = async (inputText, file, span, setMessageHistory) => {
+const handleOnClick = async (inputText, file, span, setMessageHistory, setInputText) => {
     const formData = new FormData()
     formData.append('image', file)
     formData.append('span', span)
 
     console.log(inputText);
+
+    if (!file && inputText) {
+        formData.append('prompt', inputText)
+        setMessageHistory(prevMessages => {
+            const updatedMessages = [...prevMessages, inputText];
+            return updatedMessages
+        })
+
+        const response = await axios.post(`${SERVERURL}/general`, formData);
+
+        setMessageHistory(prevMessages => {
+            const updatedMessages = [...prevMessages, response.data];
+            return updatedMessages
+        })
+    }
 
     if (file && !inputText) {
         const prompt = "Give me some travel information about the provided image"
@@ -35,7 +50,7 @@ const handleOnGenerateClick = async (inputText, file, span, setMessageHistory) =
 
     if (file && inputText) {
         formData.append('type', "General")
-        formData.append('prompt', inputText )
+        formData.append('prompt', inputText)
         setMessageHistory(prevMessages => {
             const updatedMessages = [...prevMessages, inputText];
             return updatedMessages
@@ -49,6 +64,7 @@ const handleOnGenerateClick = async (inputText, file, span, setMessageHistory) =
         })
         console.log('Image uploaded successfully:', response.data);
     }
+    setInputText("");
 }
 
 const Dashboard = () => {
@@ -80,6 +96,10 @@ const Dashboard = () => {
         }
     }
 
+    const handleChange = (e) => {
+        setInputText(e.target.value);
+    };
+
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
         console.log(event.target.files[0]);
@@ -102,19 +122,20 @@ const Dashboard = () => {
 
                         <div className="relative mt-5">
                             <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bot-message-square"><path d="M12 6V2H8" /><path d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z" /><path d="M2 12h2" /><path d="M9 11v2" /><path d="M15 11v2" /><path d="M20 12h2" /></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bot-message-square"><path d="M12 6V2H8" /><path d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z" /><path d="M2 12h2" /><path d="M9 11v2" /><path d="M15 11v2" /><path d="M20 12h2" /></svg>
                             </span>
 
                             <input
                                 type="text"
                                 className="pl-10 pr-12 py-2 border rounded-full w-full focus:outline-none focus:border-blue-500"
                                 placeholder="Enter text..."
+                                onChange={handleChange}
                             />
 
                             {/* Trailing button */}
-                            <button type="button" className="absolute inset-y-0 right-0 pr-3 flex items-center">
+                            <button type="button" className="absolute inset-y-0 right-0 pr-3 flex items-center" onClick={() => handleOnClick(inputText, selectedFile, span, setMessageHistory, setInputText)}>
                                 {/* Your trailing button content, e.g., an icon or text */}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-upload"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="17 8 12 3 7 8" /><line x1="12" x2="12" y1="3" y2="15" /></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-upload"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="17 8 12 3 7 8" /><line x1="12" x2="12" y1="3" y2="15" /></svg>
                             </button>
                         </div>
 
@@ -136,7 +157,7 @@ const Dashboard = () => {
                             </div>
                             <div className='fixed ml-96 mt-8'>
                                 {selectedFile ?
-                                    <button className={`btn`} onClick={() => handleOnGenerateClick(inputText, selectedFile, span, setMessageHistory)}>
+                                    <button className={`btn`} onClick={() => handleOnClick(inputText, selectedFile, span, setMessageHistory, setInputText)}>
                                         <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
                                             <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
                                         </svg>
