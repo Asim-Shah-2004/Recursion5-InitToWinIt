cdb267c59f7923da8fcd47efb5c492c636f4c976
Asim Shah <asimshah2111@gmail.com>
2024-03-14 03:37:07 +0530

    backend refactoring v2

--- server/controllers/handleGeneralBot.js
+++ server/controllers/handleGeneralBot.js
@@ -1,22 +1,24 @@
-import openAiBot from "../services/openAiService.js";
-
+import openAiBot from '../services/openAiService.js'
 
 const handleGeneralBot = async (req, res) => {
-    const { prompt } = req.body;
-    const result = await openAiBot(
-        ` is the question releted to travelling if releted the write only yes
-        dont write any other word otherwise answer with only no dont write any other word
-        the question is: ${prompt}
-        `
-    )
-    console.log(result)
-    if (result === 'Yes') {
-        const response = await openAiBot(
-            `${prompt}`
+    const { prompt } = req.body
+    try {
+        const result = await openAiBot(
+            ` is the question releted to travelling if releted the write only yes
+            dont write any other word otherwise answer with only no dont write any other word
+            the question is: ${prompt}
+            `
         )
-        res.send(response)
-    } else {
-        res.send(`cannot answer the following question`)
+        console.log(result)
+        if (result === 'Yes') {
+            const response = await openAiBot(`${prompt}`)
+            res.send(response)
+        } else {
+            res.send(`cannot answer the following question`)
+        }
+    } catch (error) {
+        console.log(error)
+        res.send(`error while generating prompt`)
     }
 }
 
--- server/controllers/handleSafetyConsiderations.js
+++ server/controllers/handleSafetyConsiderations.js
@@ -1,15 +1,20 @@
 import openAiBot from '../services/openAiService.js'
 
 const handleSafetyConsiderations = async (req, res, locations) => {
-    const result = await openAiBot(
-        `
-        you are a travel releated chatbot and a safety expert specialized on the location:${locations} 
-        (only consider the city name if present) 
-        and you need to tell the safety preacutions that a
-        person travelling in ${locations} must take give detailed explainations on how to impliment these measures
-        `
-    )
-    res.send(result)
+    try {
+        const result = await openAiBot(
+            `
+            you are a travel releated chatbot and a safety expert specialized on the location:${locations} 
+            (only consider the city name if present) 
+            and you need to tell the safety preacutions that a
+            person travelling in ${locations} must take give detailed explainations on how to impliment these measures
+            `
+        )
+        res.send(result)
+    } catch (error) {
+        console.log(error);
+        res.send('error while generating prompt')
+    }
 }
 
 export default handleSafetyConsiderations
