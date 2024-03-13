3927603cceefb9fb3bae7322500ea0df0e47f69a
SoulDev07 <mayankravariya@gmail.com>
2024-03-14 00:24:23 +0530

    link login and register page to backend

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react';
+// import React from 'react';
+import { useState, useEffect } from 'react';
 import { ReactTyped } from "react-typed";
 import pic1 from '../assets/pic1.jpg';
 import pic2 from '../assets/pic2.jpg';
 import pic3 from '../assets/pic3.jpg';
 import pic4 from '../assets/pic4.jpg';
 import pic5 from '../assets/pic5.jpg';
-import '../App.css';
 import { Link } from 'react-router-dom';
 import StyledButton from '@/components/ui/StyledButton';
+import '../App.css';
+
+const TEXTLIST = ["tailored itinerary", "budget analysis ", "security considerations", "clothing suggestions", "food attractions"]
 
 const LandingPage = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -30,25 +33,20 @@ const LandingPage = () => {
                         <h1 className='text-5xl p-4 font-bold text-[#FC6736]'>Travel</h1>
                         <h1 className='text-5xl ml-7 font-bold -mt-10 p-4 text-[#EFECEC]'>Smart</h1>
                     </div>
-                    
-                    
+                </div>
+
+                <div className='w-full max-w-[1400px] ml-24 mt-32 '>
+
+                    <div className='text-6xl inline text-[#EFECEC] font-sans'>We make  </div>
+                    <ReactTyped strings={TEXTLIST} typeSpeed={70} backSpeed={50} loop className='text-6xl text-[#FC6736] font-sans ' />
+                    <div className='text-6xl text-[#EFECEC] font-sans'>available for your next trip  </div>
+
+                    <div className='z-10 mt-9'>
+                        <Link to="/login"> <StyledButton /> </Link>
+                    </div>
 
                 </div>
-                <div className='w-full max-w-[1400px] ml-24 mt-32 '>
-                    <div className='text-6xl inline text-[#EFECEC] font-sans'>We make  </div>
-                    <ReactTyped strings={["tailored itinerary", "budget analysis ", "security considerations", "clothing suggestions", "food attractions"]} typeSpeed={70} backSpeed={50} loop className='text-6xl text-[#FC6736] font-sans ' />
-                    <div className='text-6xl text-[#EFECEC] font-sans'>available for your next trip  </div>
-                    <div className='z-10 mt-9  '>
-                        
-                        <StyledButton />
-                        
-                    </div>
-                </div>
-                <img
-                    src={images[currentImageIndex]}
-                    alt=""
-                    className='sideImage fixed -z-1 select-none'
-                />
+                <img src={images[currentImageIndex]} className='sideImage fixed -z-1 select-none' alt="" />
             </div>
         </>
     );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
-// import down from "../assets/down.png";
+// import React from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-
+const SERVERURL = import.meta.env.VITE_SERVERURL;
 
 const Login = () => {
-    const navigate = useNavigate();
+    const [username, setUsername] = useState(localStorage.getItem("username") || "");
+    const [password, setPassword] = useState(localStorage.getItem("password") || "");
     const [registerError, setRegisterError] = useState(false);
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState("Enter Valid Details!");
+    const navigate = useNavigate();
 
     const [formData, setFormData] = useState({
         username: "",
         password: "",
     });
+
+    useEffect(() => {
+        const checkLogin = async () => {
+            try {
+                const response = await axios.post(`${SERVERURL}/login`, { username: username, password: password, });
+                if (response.data.user === username)
+                    navigate('/dashboard');
+            } catch (error) {
+                console.error('Login failed:', error);
+            }
+        };
+
+        checkLogin();
+    }, [])
+
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -23,29 +40,35 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post(
-                `http://localhost:3000/login`,formData);
-            console.log(response.data); // You can handle the response as needed
-            navigate('/dashboard');
+            const response = await axios.post(`${SERVERURL}/login`, formData);
+            if (response.data.user === formData.username) {
+                localStorage.clear();
+                localStorage.setItem("username", response.data.user);
+                localStorage.setItem("password", response.data.password);
 
-        } catch (error) {
-            console.error("Error login:", error);
+                navigate('/dashboard');
+            }
+            else
+                navigate('/');
+        } catch (err) {
+            console.error("Error login:", err);
             setRegisterError(true);
-            setMessage(error.response.data.fetch);
-            console.log(error.response.data.fetch);
+            setMessage(err.response.data.message);
+            // console.log(error.response.data);
         }
     };
-
 
     return (
         <div className="main flex mx-auto items-center justify-between h-screen flex-col bg-slate-100">
             <div className="flex mx-auto items-center justify-end md:justify-center h-screen flex-col">
                 <h1 className="text-6xl mb-7 mainText font-bold uppercase text-red-700">fkjdkjfkd</h1>
+
                 {registerError && (
                     <p className="text-white bg-red-500 p-3 m-3 rounded-full">
                         {message}
                     </p>
                 )}
+
                 <form
                     className="flex flex-col justify-center items-center"
                     onSubmit={handleSubmit}
@@ -71,9 +94,9 @@ const Login = () => {
                         Login
                     </button>
                     <div className="mt-4">
-                        Not our part yet ?
-                        <Link to="/">
-                            <div className=" inline text-red-700">Sign Up</div>
+                        Not our part yet?
+                        <Link to="/register">
+                            <div className=" inline text-red-700"> Sign Up </div>
                         </Link>
                     </div>
                 </form>
