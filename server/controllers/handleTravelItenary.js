a0d98c53d8074add9d6fc3c8958dc77dadb38e5a
Asim Shah <asimshah2111@gmail.com>
2024-03-13 15:38:21 +0530

    added handle travel itenary

---
+++ server/controllers/handleTravelItenary.js
@@ -0,0 +1,7 @@
+import openAiBot from '../services/openAiService'
+
+const handleTravelItenary = (req, res) => {
+    const { location } = req.body
+}
+
+export default handleTravelItenary
--- server/services/openAiService.js
+++ server/services/openAiService.js
@@ -1,17 +1,17 @@
-import openai from "../config/openaiOptions.js";
+import openai from '../config/openaiOptions.js'
 
-const openAiBot = async(req,res)=>{
-    const {prompt} = req.body;
-    try{
+const openAiBot = async (req, res) => {
+    const { prompt } = req.body
+    try {
         const response = await openai.chat.completions.create({
-            messages:[{role:'user',content:`${prompt}`}],
-            model:'gpt-3.5-turbo'
+            messages: [{ role: 'user', content: `${prompt}` }],
+            model: 'gpt-3.5-turbo',
         })
         const result = response.choices[0].message.content
-        res.send({message:result})
-    }catch(err){
+        res.send({ message: result })
+    } catch (err) {
         console.log(err)
-        res.send({message:"error in openAi bot"})
+        res.send({ message: 'error in openAi bot' })
     }
 }
 
