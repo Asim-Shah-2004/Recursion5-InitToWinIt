6019352a926416c2015124a32f603739f6c9cb57
Asim Shah <asimshah2111@gmail.com>
2024-03-14 07:52:22 +0530

    generel bot completed but avoid using

--- server/controllers/handleGeneralBot.js
+++ server/controllers/handleGeneralBot.js
@@ -1,27 +1,35 @@
 import openAiBot from '../services/openAiService.js'
-
+import openai from '../config/openaiOptions.js'
 const handleGeneralBot = async (req, res) => {
     const { prompt } = req.body
     try {
-        // const result = await openAiBot(
-        //     ` "Is the question related to travel? 
-        //      ${prompt}" 
-        //     Respond with 'yes' if the question is related to travel; 
-        //     otherwise, respond with 'no' (omit any other words).
-        //     `
-        // )
-        // console.log(result)
-        // if (result.toLocaleLowerCase() === 'yes') {
-        //     console.log('awaiting openai');
-        //     const response = await openAiBot(`
-        //     ${prompt}`)
-        //     res.send(response)
-        // } else {
-        //     res.send(`cannot answer the following question`)
-        // }
-        const result = await openAiBot(`please answer this question :${prompt}`)
+        const result = await openAiBot(
+            ` "Is the question related to travel and tourism? 
+             ${prompt}" 
+            Respond with 'yes' if the question is related to travel or tourism; 
+            otherwise, respond with 'no' (omit any other words).
+            `
+        )
         console.log(result)
-        res.send(result)
+        if (result.toLocaleLowerCase() === 'yes') {
+            console.log('awaiting openai');
+            try{
+                const response = await openai.chat.completions.create({
+                    messages: [{ role: 'user', content: `answer this question : ${prompt}?` }],
+                    model: 'gpt-3.5-turbo',
+                })
+        
+                const result = response.choices[0].message.content
+                console.log(result);
+                res.send(result)
+            }catch(err){
+                console.log(err);
+                res.send('error while generating prompt')
+            }
+        } else {
+            res.send(`cannot answer the following question`)
+        }
+
     } catch (error) {
         console.log(error)
         res.send(`error while generating prompt`)
